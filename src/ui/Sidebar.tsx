578f9bf1fcade0dd6cd7228876b6f97848df309d
Soe Wei Yan Phyo <soeweiyan.phyo@gmail.com>
2025-05-31 10:43:21 +1000

    feat: Iimplement sidebar navigation

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { Logo } from './Logo'
+import { MainNav } from './MainNav'
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -9,5 +11,10 @@ const StyledSidebar = styled.aside`
 `
 
 export function Sidebar() {
-  return <StyledSidebar>Sidebar</StyledSidebar>
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  )
 }
