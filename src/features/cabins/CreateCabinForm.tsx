ba82020ab935890842ef8b0e1f9c9296d1fcc409
Soe Wei Yan Phyo <soeweiyan.phyo@gmail.com>
2025-06-08 00:54:09 +1000

    refactor: remove unused styled components from CreateCabinForm

--- src/features/cabins/CreateCabinForm.tsx
+++ src/features/cabins/CreateCabinForm.tsx
@@ -12,42 +12,6 @@ import { Textarea } from '@/ui/Textarea'
 import { createCabin } from '@/services/apiCabins'
 import { FormRow } from '@/ui/FormRow'
 
-const FormRow2 = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`
-
-const Label = styled.label`
-  font-weight: 500;
-`
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`
-
 type FormInputs = Omit<Cabin, 'id' | 'created_at' | 'image'>
 
 enum FormFields {
